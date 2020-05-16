759615bca90b29f0ea72cc78f7f54cf29b1f938b
airix-png <pngquach@gmail.com>
2020-05-15 23:54:54 -0700

    ver 2.0 put everything into switch

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const btn2 = document.querySelector ('#shading');
 const btn3 = document.querySelector ('#spectrum');
 const btn4 = document.querySelector ('#erase');
 const btn5 = document.querySelector ('#refresh');
+const btn6 = document.querySelector ('#gridlines')
 
 //Generate Grid
 function createGrid(num) {
@@ -16,9 +17,8 @@ function createGrid(num) {
     for (let c = 0; c < (num * num); c++) {
         const box = document.createElement('div');
         box.classList.add("square");
-        box.addEventListener("mouseover", function (e) {
-            box.style.backgroundColor = "#434C5E";
-        })
+        box.addEventListener("mouseover", final
+        )
             squareGrid.appendChild(box);
     }
 }
@@ -46,46 +46,62 @@ canvas.addEventListener('click', function(e){
 
 
                     //BUTTONS
-//nord
-btn1.addEventListener('click', function (e){
+let mode=1
+function final(){
     const gridSquares = document.querySelectorAll('.square');
-    gridSquares.forEach(box => box.addEventListener('mouseover', function nord(e) {
-        box.style.opacity = 1;
-        box.style.backgroundColor = "#434C5E";
-}))
-})
-//graphite
-btn2.addEventListener('click', function (e){
-    const gridSquares = document.querySelectorAll('.square');
-    gridSquares.forEach(box => box.addEventListener("mouseover", function shading(e){
-        let opacity = Number(box.style.opacity);
+    gridSquares.forEach(box => box.addEventListener("mouseover", function (e) {
+    switch(mode){
+    case 1: {box.style.opacity = 1;
+        box.style.backgroundColor = "#434C5E";}
+        break;
+    case 2: {let opacity = Number (box.style.opacity);
         box.style.opacity = opacity += 0.1;
-        box.style.backgroundColor = '#000';
-        box.style.transition = 'background-color 0.25s';
-		}
+        box.style.backgroundColor = 'black';}
+    break;
+    case 3: {
+        box.style.opacity = 1;
+        box.style.backgroundColor = getRandColor();}
+        break;
+    case 4: { 
+        box.style.background = "#ECEFF4"
+        break;}
+    }}
+))}
+function shadedColor(box) {
+    if (!box.style.background.includes("rgba")) {
+        console.log("doesnt include");
+        return "rgba(0,0,0,0.1)";
+    }
+    let alpha = box.style.background.split(',')[3];
+    alpha = alpha.substr(0, alpha.length - 1);
+    alpha = +alpha;
+    alpha += 0.1;
+    if (alpha >= 1) alpha = 0.99; // -> so ugly (for some reason setting rgba to alpha 1 it gets set to rgb without alpha)
+    console.log(alpha);
+    return "rgba(0,0,0," + alpha + ")";
+}
 
-))})
+//nord
+btn1.addEventListener('click', function nord(e){mode=1})
+//graphite
+btn2.addEventListener('click', function shade(e){mode=2;
+    lightness = 80})
     
 //spectrum
-btn3.addEventListener('click', function (e){
-    const gridSquares = document.querySelectorAll('.square');
-    gridSquares.forEach(box => box.addEventListener("mouseover", function spectrum(e){
-        box.style.opacity = 1;
-        box.style.backgroundColor = getRandColor();
-    }))
-})
+btn3.addEventListener('click', function spectrum(e){mode=3})
 //erase
-btn4.addEventListener('click', function (e){
-    const gridSquares = document.querySelectorAll('.square');
-    gridSquares.forEach(box => box.addEventListener("mouseover",function (e) {
-        box.style.background = "#ECEFF4"
-    }))
-})
+btn4.addEventListener('click', function erase(e){mode=4})
+
+
 //refresh
 btn5.addEventListener('click', function(e){
     const gridSquares = document.querySelectorAll('.square');
     gridSquares.forEach(box => box.style.backgroundColor = "");
 })
 
+btn6.addEventListener('click', function(e){
+	const gridSquares = document.querySelectorAll('.square');
+	gridSquares.forEach(box => box.classList.toggle('grid-lines'));
+})
 
 createGrid(16)
